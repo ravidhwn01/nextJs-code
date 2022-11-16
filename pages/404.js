01e88725a1327d15d67b7df2f26133bab03969c3
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-16 16:22:09 +0530

    redirect to Home page using useEffect  in next js

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,19 @@
 // 404 error page
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 function error() {
   const router = useRouter();
+  useEffect(()=>{
+    setTimeout(()=>{ 
+      router.push('/')   // redirecting to home page after few second
+    }, 3000)
+  },[]);
 
   return (
     <>
       <h1>404 error page! sorry</h1>
       {/* it will route to home page without loading the page */}
-      <a onClick={() => router.push("/")}> go to home</a>
+      {/* <a onClick={() => router.push("/")}> go to home</a> */}
     </>
   );
 }
